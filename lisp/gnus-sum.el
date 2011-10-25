d592041a409a20b6bdeb4c5fb9552a002d63196a
Dave Abrahams <dave@boostpro.com>
2011-10-24 20:05:04 -0400

    Record information in the registry about each article retrieved

--- lisp/gnus-sum.el
+++ lisp/gnus-sum.el
@@ -12384,6 +12384,13 @@ If REVERSE, save parts that do not match TYPE."
 		(not (setq header (car (gnus-get-newsgroup-headers nil t)))))
 	    ()				; Malformed head.
 	  (unless (gnus-summary-article-sparse-p (mail-header-number header))
+            (when (and (bound-and-true-p gnus-registry-enabled)
+                       (not (gnus-ephemeral-group-p (car where))))
+              (gnus-registry-handle-action
+               (mail-header-id header) nil
+               (gnus-group-prefixed-name (car where) gnus-override-method)
+               (mail-header-subject header)
+               (mail-header-from header)))
 	    (when (and (stringp id)
 		       (or
 			(not (string= (gnus-group-real-name group)
